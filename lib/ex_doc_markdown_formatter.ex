a0c00ca1df4524ed2fc2db433ed6b367ec965cac
Johan Wärlander <johan@snowflake.nu>
2018-08-13 16:10:27 +0200

    ExDoc Markdown output, based on ExDoc.Formatter.HTML

--- lib/ex_doc_markdown_formatter.ex
+++ lib/ex_doc_markdown_formatter.ex
@@ -1,18 +1,183 @@
 defmodule ExDocMarkdownFormatter do
   @moduledoc """
-  Documentation for ExDocMarkdownFormatter.
+  Generates Markdown documentation for Elixir projects.
   """
+
+  alias __MODULE__.{Autolink, Templates}
+  alias ExDoc.GroupMatcher
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ExDocMarkdownFormatter.hello
-      :world
-
+  Generate Markdown documentation for the given modules.
   """
-  def hello do
-    :world
+  @spec run(list, ExDoc.Config.t()) :: String.t()
+  def run(project_nodes, config) when is_map(config) do
+    config = %{config | output: Path.expand(config.output)}
+
+    build = Path.join(config.output, ".build")
+    output_setup(build, config)
+
+    autolink = Autolink.compile(project_nodes, ".html", config.deps)
+    linked = Autolink.all(project_nodes, autolink)
+
+    nodes_map = %{
+      modules: filter_list(:module, linked),
+      exceptions: filter_list(:exception, linked),
+      tasks: filter_list(:task, linked)
+    }
+
+    extras = build_extras(config, autolink)
+
+    generated_files =
+      generate_extras(nodes_map, extras, config) ++
+        generate_list(nodes_map.modules, nodes_map, config) ++
+        generate_list(nodes_map.exceptions, nodes_map, config) ++
+        generate_list(nodes_map.tasks, nodes_map, config)
+
+    generate_build(generated_files, build)
+    config.output |> Path.relative_to_cwd()
+  end
+
+  defp output_setup(build, config) do
+    if File.exists?(build) do
+      build
+      |> File.read!()
+      |> String.split("\n", trim: true)
+      |> Enum.map(&Path.join(config.output, &1))
+      |> Enum.each(&File.rm/1)
+
+      File.rm(build)
+    else
+      File.rm_rf!(config.output)
+      File.mkdir_p!(config.output)
+    end
+  end
+
+  defp generate_build(files, build) do
+    entries = Enum.map(files, &[&1, "\n"])
+    File.write!(build, entries)
+  end
+
+  defp generate_extras(_nodes_map, extras, config) do
+    Enum.map(extras, fn %{id: id, content: content} ->
+      filename = "#{id}.md"
+      output = "#{config.output}/#{filename}"
+
+      if File.regular?(output) do
+        IO.puts(:stderr, "warning: file #{Path.relative_to_cwd(output)} already exists")
+      end
+
+      File.write!(output, content)
+      filename
+    end)
+  end
+
+  @doc """
+  Builds extra nodes by normalizing the config entries.
+  """
+  def build_extras(config, autolink) do
+    groups = config.groups_for_extras
+
+    config.extras
+    |> Task.async_stream(&build_extra(&1, autolink, groups), timeout: :infinity)
+    |> Enum.map(&elem(&1, 1))
+    |> Enum.sort_by(fn extra -> GroupMatcher.group_index(groups, extra.group) end)
+  end
+
+  defp build_extra({input, options}, autolink, groups) do
+    input = to_string(input)
+    id = options[:filename] || input |> input_to_title() |> title_to_id()
+    build_extra(input, id, options[:title], autolink, groups)
+  end
+
+  defp build_extra(input, autolink, groups) do
+    id = input |> input_to_title() |> title_to_id()
+    build_extra(input, id, nil, autolink, groups)
+  end
+
+  defp build_extra(input, id, title, autolink, groups) do
+    if valid_extension_name?(input) do
+      content =
+        input
+        |> File.read!()
+        |> Autolink.project_doc(autolink)
+
+      group = GroupMatcher.match_extra(groups, input)
+
+      title = title || extract_title(content) || input_to_title(input)
+      %{id: id, title: title, group: group, content: content}
+    else
+      raise ArgumentError, "file format not recognized, allowed format is: .md"
+    end
+  end
+
+  def valid_extension_name?(input) do
+    file_ext =
+      input
+      |> Path.extname()
+      |> String.downcase()
+
+    if file_ext in [".md"] do
+      true
+    else
+      false
+    end
+  end
+
+  @h1_regex ~r/^#\s+(.+)\s+$/m
+  defp extract_title(content) do
+    title = Regex.run(@h1_regex, content, capture: :all_but_first)
+
+    if title do
+      title |> List.first() |> String.trim()
+    end
+  end
+
+  @doc """
+  Convert the input file name into a title_to_filename/1
+  """
+  def input_to_title(input) do
+    input |> Path.basename() |> Path.rootname()
+  end
+
+  @doc """
+  Creates an ID from a given title
+  """
+  def title_to_id(title) do
+    title |> String.replace(" ", "-") |> String.downcase()
+  end
+
+  def filter_list(:module, nodes) do
+    Enum.filter(nodes, &(not (&1.type in [:exception, :impl, :task])))
+  end
+
+  def filter_list(type, nodes) do
+    Enum.filter(nodes, &(&1.type == type))
+  end
+
+  defp generate_list(nodes, nodes_map, config) do
+    nodes
+    |> Task.async_stream(&generate_module_page(&1, nodes_map, config), timeout: :infinity)
+    |> Enum.map(&elem(&1, 1))
+  end
+
+  defp generate_module_page(module_node, nodes_map, config) do
+    filename = "#{module_node.id}.md"
+    config = set_canonical_url(config, filename)
+    content = Templates.module_page(module_node, nodes_map, config)
+    File.write!("#{config.output}/#{filename}", content)
+    filename
+  end
+
+  defp set_canonical_url(config, filename) do
+    if config.canonical do
+      canonical_url =
+        config.canonical
+        |> String.trim_trailing("/")
+        |> Path.join(filename)
+
+      Map.put(config, :canonical, canonical_url)
+    else
+      config
+    end
   end
 end
---
+++ lib/ex_doc_markdown_formatter/templates.ex
@@ -0,0 +1,126 @@
+defmodule ExDocMarkdownFormatter.Templates do
+  @moduledoc false
+  require EEx
+
+  @doc """
+  Generate content from the module template for a given `node`
+  """
+  def module_page(module_node, nodes_map, config) do
+    summary_map = module_summary(module_node)
+    module_template(config, module_node, summary_map, nodes_map)
+  end
+
+  @doc """
+  Get the full specs from a function, already in HTML form.
+  """
+  def get_specs(%ExDoc.TypeNode{spec: spec}) do
+    [spec]
+  end
+
+  def get_specs(%ExDoc.FunctionNode{specs: specs}) when is_list(specs) do
+    presence(specs)
+  end
+
+  def get_specs(_node) do
+    nil
+  end
+
+  @doc """
+  Get defaults clauses.
+  """
+  def get_defaults(%{defaults: defaults}) do
+    defaults
+  end
+
+  def get_defaults(_) do
+    []
+  end
+
+  @doc """
+  Generate a link id
+  """
+  def link_id(module_node), do: link_id(module_node.id, module_node.type)
+
+  def link_id(id, type) do
+    case type do
+      :macrocallback -> "c:#{id}"
+      :callback -> "c:#{id}"
+      :type -> "t:#{id}"
+      :opaque -> "t:#{id}"
+      _ -> "#{id}"
+    end
+  end
+
+  @doc """
+  Returns the HTML formatted title for the module page.
+  """
+  def module_title(%{type: :task, title: title}),
+    do: "mix " <> title
+
+  def module_title(%{type: :module, title: title}),
+    do: title
+
+  def module_title(%{type: type, title: title}),
+    do: title <> " <small>#{type}</small>"
+
+  @doc """
+  Gets the first paragraph of the documentation of a node. It strips
+  surrounding spaces and strips traling `:` and `.`.
+
+  If `doc` is `nil`, it returns `nil`.
+  """
+  @spec synopsis(String.t()) :: String.t()
+  @spec synopsis(nil) :: nil
+
+  def synopsis(nil), do: nil
+  def synopsis(""), do: ""
+
+  def synopsis(doc) when is_binary(doc) do
+    doc
+    |> String.split(~r/\n\s*\n/)
+    |> hd()
+    |> String.trim()
+    |> String.replace(~r{[.:\s]+$}, "")
+    |> String.trim_trailing()
+  end
+
+  defp presence([]), do: nil
+  defp presence(other), do: other
+
+  @doc false
+  def h(binary) do
+    escape_map = [{"&", "&amp;"}, {"<", "&lt;"}, {">", "&gt;"}, {~S("), "&quot;"}]
+
+    Enum.reduce(escape_map, binary, fn {pattern, escape}, acc ->
+      String.replace(acc, pattern, escape)
+    end)
+  end
+
+  @doc false
+  def enc_h(binary) do
+    binary
+    |> URI.encode()
+    |> h()
+  end
+
+  def module_summary(module_node) do
+    %{
+      callbacks: Enum.filter(module_node.docs, &(&1.type in [:callback, :macrocallback])),
+      functions: Enum.filter(module_node.docs, &(&1.type in [:function, :macro])),
+      guards: Enum.filter(module_node.docs, &(&1.type in [:guard])),
+      types: module_node.typespecs
+    }
+  end
+
+  templates = [
+    detail_template: [:module_node, :_module],
+    footer_template: [:config],
+    module_template: [:config, :module, :summary_map, :nodes_map]
+  ]
+
+  Enum.each(templates, fn {name, args} ->
+    filename = Path.expand("templates/#{name}.eex", __DIR__)
+    @doc false
+    EEx.function_from_file(:def, name, filename, args, trim: true)
+  end)
+end
